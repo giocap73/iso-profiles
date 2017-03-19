f6aa5faa704b23d621adee172bee0a8883b17712
Giovanni Cappella <giocap73@users.noreply.github.com>
2017-03-19 10:34:53 +0100

    for vmware workstation

---
+++ community/budgie/wm.sh
@@ -0,0 +1,11 @@
+#come installare vmware workstation
+
+#https://wiki.archlinux.org/index.php/VMware
+
+   # sh vmware.bundle
+ cd  /etc/init.d 
+ vmware-modconfig --console --install-all
+# serial key 5A02H-AU243-TZJ49-GTC7K-3C61N 
+export VMWARE_USE_SHIPPED_LIBS='yes'
+vmware
+
